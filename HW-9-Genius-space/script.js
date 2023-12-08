3c2b813a73abd60dc5e9584ab8723672312d1524
Ira An <anurovapr@gmail.com>
2023-12-08 13:50:30 +0200

    9 Homework

---
+++ HW-9-Genius-space/script.js
@@ -0,0 +1,27 @@
+// -----1----
+// Напишіть такий JavaScript, щоб після натискання на кнопку button, елемент
+// <div id="text"> зникав.
+
+// const buttonHide = document.querySelector('#btn1');
+// const child = document.querySelector('#text');
+// const parent = document.body;
+
+// buttonHide.addEventListener('click', () => 
+// parent.removeChild(child));
+
+
+
+// -----2----
+// Напишіть такий код, щоб після натискання на кнопку, вона зникала.
+
+// const button = document.querySelector('#btn1');
+// button.addEventListener ('click', () => 
+// document.getElementById("btn1").style.visibility = "hidden");
+
+// const button = document.querySelector('#btn1');
+// button.addEventListener ('click', () => 
+// document.getElementById("btn1").style.display = "none");
+
+// -----3----
+// Створіть дерево, яке показує/приховує дочірні вузли при кліці
+
